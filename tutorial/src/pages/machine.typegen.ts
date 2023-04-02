552dedcb79d12de72de78a12f6abb3ac99d5d8e3
Harold <harold.marino@iag.com.au>
2023-04-02 18:41:45 +1000

    reformat

--- tutorial/src/pages/machine.typegen.ts
+++ tutorial/src/pages/machine.typegen.ts
@@ -1,37 +1,36 @@
+// This file was automatically generated. Edits will be overwritten
 
-  // This file was automatically generated. Edits will be overwritten
-
-  export interface Typegen0 {
-        '@@xstate/typegen': true;
-        internalEvents: {
-          "xstate.init": { type: "xstate.init" };
-"xstate.stop": { type: "xstate.stop" };
-        };
-        invokeSrcNameMap: {
-          
-        };
-        missingImplementations: {
-          actions: "Mute microphone" | "Unimite microphone" | "Unmute microphone";
-          delays: never;
-          guards: never;
-          services: never;
-        };
-        eventsCausingActions: {
-          "Mute microphone": "Mute" | "xstate.init";
-"Unimite microphone": "Mute" | "xstate.stop";
-"Unmute microphone": "Mute";
-        };
-        eventsCausingDelays: {
-          
-        };
-        eventsCausingGuards: {
-          
-        };
-        eventsCausingServices: {
-          
-        };
-        matchesStates: "Microfone" | "Microfone.Muted" | "Microfone.Unmuted" | "Video" | "Video.Hiding video" | "Video.Showing video" | { "Microfone"?: "Muted" | "Unmuted";
-"Video"?: "Hiding video" | "Showing video"; };
-        tags: never;
-      }
-  
+export interface Typegen0 {
+  "@@xstate/typegen": true;
+  internalEvents: {
+    "xstate.init": { type: "xstate.init" };
+    "xstate.stop": { type: "xstate.stop" };
+  };
+  invokeSrcNameMap: {};
+  missingImplementations: {
+    actions: "Mute microphone" | "Unmute microphone" | "Unmute microphone";
+    delays: never;
+    guards: never;
+    services: never;
+  };
+  eventsCausingActions: {
+    "Mute microphone": "Mute" | "xstate.init";
+    "Unmute microphone": "Mute" | "xstate.stop";
+    "Unmute microphone": "Mute";
+  };
+  eventsCausingDelays: {};
+  eventsCausingGuards: {};
+  eventsCausingServices: {};
+  matchesStates:
+    | "Microfone"
+    | "Microfone.Muted"
+    | "Microfone.Unmuted"
+    | "Video"
+    | "Video.Hiding video"
+    | "Video.Showing video"
+    | {
+        Microfone?: "Muted" | "Unmuted";
+        Video?: "Hiding video" | "Showing video";
+      };
+  tags: never;
+}
--- tutorial/src/pages/state-machine/index.tsx
+++ tutorial/src/pages/state-machine/index.tsx
@@ -1,44 +1,52 @@
-import { useEffect, useState } from "react"
-import {useMachine} from '@xstate/react'
-import {machine2} from '../machine'
+import { useEffect, useState } from "react";
+import { useMachine } from "@xstate/react";
+import { machine2 } from "../machine";
 
 const consoleLogTransition = () => {
-    const newState = machine2.transition(machine2.initialState, {
-        type: "Mute"
-    })
-    console.log(newState.value, newState.context)
-}
+  const newState = machine2.transition(machine2.initialState, {
+    type: "Unmute",
+  });
+  console.log(
+    "Transition->",
+    newState.value,
+    newState.context,
+    newState.actions
+  );
+
+  console.log(newState);
+
+  if (newState.matches("Microphone.Unmuted")) console.log("WOOOOOW");
+};
 
 const StateMachine = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [state, send] = useMachine(machine2)
-    console.log(state.value)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [state, send] = useMachine(machine2);
 
-    useEffect(() => {
-        consoleLogTransition()
-    }, [])
+  useEffect(() => {
+    consoleLogTransition();
+  }, []);
 
-    return (
-        <div>
-            <pre style={{marginBottom: "2rem"}}>
-                {JSON.stringify({
-                    value: state.value,
-                    context: state.context
-                },
-                null,
-                2)}
-            </pre>
-            {state.nextEvents.map((event, idx)=>{
-                return(
-                    <div key={idx} style={{marginBottom: "1rem"}}>
-                        <button onClick={()=> send(event)}>
-                            {event}
-                        </button>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div>
+      <pre style={{ marginBottom: "2rem" }}>
+        {JSON.stringify(
+          {
+            value: state.value,
+            context: state.context,
+          },
+          null,
+          2
+        )}
+      </pre>
+      {state.nextEvents.map((event, idx) => {
+        return (
+          <div key={idx} style={{ marginBottom: "1rem" }}>
+            <button onClick={() => send(event)}>{event}</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default StateMachine
+export default StateMachine;
