2d8e6639abe44df4dedc80567c5793149961de03
Harold <harold.marino@iag.com.au>
2023-04-02 11:21:32 +1000

    initial state machine and nextjs

---
+++ tutorial/src/pages/machine.typegen.ts
@@ -0,0 +1,37 @@
+
+  // This file was automatically generated. Edits will be overwritten
+
+  export interface Typegen0 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "xstate.init": { type: "xstate.init" };
+"xstate.stop": { type: "xstate.stop" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: "Mute microphone" | "Unimite microphone" | "Unmute microphone";
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          "Mute microphone": "Mute" | "xstate.init";
+"Unimite microphone": "Mute" | "xstate.stop";
+"Unmute microphone": "Mute";
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: "Microfone" | "Microfone.Muted" | "Microfone.Unmuted" | "Video" | "Video.Hiding video" | "Video.Showing video" | { "Microfone"?: "Muted" | "Unmuted";
+"Video"?: "Hiding video" | "Showing video"; };
+        tags: never;
+      }
+  
